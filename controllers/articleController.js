ec6ffc72a852dd911fa068f5cf642ae3199e0797
rushi mungse <mungse.rushi@gmail.com>
2022-01-29 23:16:39 +0530

    get, patch and upadete route created

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,36 +1,60 @@
 const Articles = require("../models/article");
 
+//Custom apis
+
 class ArticleController {
-  //Custom apis
+  //post-articles
   async article(req, res) {
     const { title, body, author } = req.body;
-    console.log(req.body);
     const articles = new Articles({
       title,
       body,
       author,
     });
 
-    //first method
-    // articles.save((err, document) => {
-    //   if (err) {
-    //     throw err;
-    //   }
-    //   res.json(document);
-    // });
+    try {
+      const document = await articles.save();
+      res.status(201).json(document);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-    //Second Method
-    // articles
-    //   .save()
-    //   .then((document) => {
-    //     res.status(201).json(document);
-    //   })
-    //   .catch((err) => {
-    //     throw err;
-    //   });
+  //get articles
+  async getArticles(req, res, next) {
+    try {
+      const data = await Articles.find();
+      res.status(201).json(data);
+    } catch (error) {
+      res.status(400).json({ Error: "something went wrong!" });
+    }
+  }
 
-    const document = await articles.save();
-    res.status(201).json(document);
+  //update-articles
+  async updateArticles(req, res, next) {
+    const { _id } = req.params;
+    const { title, body, author } = req.body;
+    if (!_id) {
+      return res.status(400).json({ Error: "Id not font" });
+    }
+    try {
+      const exist = await Articles.findOne({ _id });
+      if (!exist) {
+        res.status(400).json({ Error: "Article not found" });
+      }
+      const updateArticle = await Articles.updateOne(
+        { _id },
+        {
+          title,
+          body,
+          author,
+        }
+      );
+      res.status(201).json(req.body);
+    } catch (err) {
+      console.log(err);
+      res.json({ Error: "Something went wrong!" });
+    }
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ const routers = require("express").Router();
 const articleController = require("../controllers/articleController");
 
 routers.post("/api/article", articleController.article);
+routers.get("/api/article", articleController.getArticles);
+routers.patch("/api/article/:_id", articleController.updateArticles);
 
 module.exports = routers;
